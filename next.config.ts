3a63ff267e89b7831e3b6a3da1e271ebba351d7e
Carry Bit <106582425+carrybit-dev@users.noreply.github.com>
2026-09-22 02:26:28 +0600

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
-  },
-  // Onnanno kono setting thakle shegulo temoni thakbe
+  }
 };
 
 export default nextConfig;
